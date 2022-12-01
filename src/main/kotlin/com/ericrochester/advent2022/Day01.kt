6ee9128e13c82d5f4af00a8274f4c1f49f3b6b62
Eric Rochester <erochest@gmail.com>
2022-12-01 16:27:45 -0500

    refactored out paragraph splitting

--- src/main/kotlin/com/ericrochester/advent2022/Day01.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day01.kt
@@ -2,16 +2,30 @@ package com.ericrochester.advent2022
 
 class Day01 {
     fun runA(inputData: String): Int {
+        val maxCalorieSums = paragraphs(inputData.lines()).map {
+            val sum = it.sumOf { line: String -> line.toInt() }
+            println(sum)
+            sum
+        }.sortedDescending<Int>()
+        println(maxCalorieSums)
+        return maxCalorieSums.first()
+    }
+
+    fun runB(inputData: String): Int {
         var lines = inputData.lines()
-        var maxCalorieSum = 0
-        while (!lines.isEmpty()) {
+        return 0
+    }
+
+    fun paragraphs(lines: List<String>): List<List<String>> {
+        var lines = lines
+        var output = emptyList<List<String>>().toMutableList()
+        while (lines.isNotEmpty()) {
             val chunk = lines.takeWhile { it.isNotEmpty() }
             lines = lines.drop(chunk.size)
-            val chunkSum = chunk.map { it.toInt() }.sum()
-            maxCalorieSum = maxOf(maxCalorieSum, chunkSum)
+            output.add(chunk)
             lines = lines.drop(lines.takeWhile { it.isEmpty() }.size)
         }
-        return maxCalorieSum
+        return output
     }
 
 }
--- src/test/kotlin/com/ericrochester/advent2022/TestDay01.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay01.kt
@@ -4,11 +4,31 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TestDay01 {
+    private val inputData = javaClass.getResource("/examples/day01.txt").readText()
+    private val day01 = Day01()
+
     @Test
     fun testRunA() {
-        val inputData = javaClass.getResource("/examples/day01.txt").readText()
-        val day01 = Day01()
         val output = day01.runA(inputData)
         assertEquals(24000, output)
+    }
+
+    @Test
+    fun testParagraphs() {
+        val paras = day01.paragraphs(inputData.lines())
+        val expected = listOf(
+            listOf("1000", "2000", "3000"),
+            listOf("4000"),
+            listOf("5000", "6000"),
+            listOf("7000", "8000", "9000"),
+            listOf("10000")
+        )
+        assertEquals(expected, paras)
+    }
+
+    @Test
+    fun testRunB() {
+        val output = day01.runB(inputData)
+        assertEquals(45000, output)
     }
 }
