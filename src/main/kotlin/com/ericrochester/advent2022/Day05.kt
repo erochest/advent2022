06372bc25c51e679382b5f01223aaec6ba380da6
Eric Rochester <erochest@gmail.com>
2022-12-05 12:55:23 -0500

    day 5 a

--- src/main/kotlin/com/ericrochester/advent2022/Day05.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day05.kt
@@ -2,11 +2,90 @@ package com.ericrochester.advent2022
 
 class Day05: DayRuns<String, Int> {
     override fun runA(inputData: String): String {
-        TODO("Not yet implemented")
+        var lines = inputData.lines()
+        val stacks = parseStacks(lines.takeWhile { it.isNotBlank() })
+        val stackMoves = lines
+            .takeLastWhile { it.isNotBlank() }
+            .map { parseStackMove(it) }
+            .forEach {
+//                println("MOVE ${it} -> ${stacks}")
+                stacks.makeStackMove(it)
+            }
+
+        return stacks.peekAll().joinToString("")
     }
 
     override fun runB(inputData: String): Int {
         TODO("Not yet implemented")
     }
 
+    fun parseStacks(input: List<String>): ElfStacks {
+        val lastLine = input.last()
+        val stackHeaders = lastLine.split("\\s+".toRegex()).filter { it.isNotBlank() }
+//        println("headers = ${stackHeaders}")
+        val stacks = Array(stackHeaders.size) { ArrayDeque<Char>() }
+
+        input.dropLast(1)
+            .forEach { line: String ->
+                line.chunked(4)
+                    .forEachIndexed { index, s ->
+                        if (s.isNotBlank()) {
+                            val c = s[1]
+                            stacks[index].add(0, c)
+                        }
+                    }
+            }
+
+        return ElfStacks.withStacks(*stacks)
+    }
+
+    fun parseStackMove(input: String): StackMove {
+        val parts = input.split("\\s+".toRegex())
+        return StackMove(
+            parts[1].toInt(),
+            parts[3].toInt(),
+            parts[5].toInt()
+        )
+    }
 }
+
+class ElfStacks {
+    var stacks: List<ArrayDeque<Char>> = emptyList()
+
+    companion object {
+        fun withStacks(vararg stacks: ArrayDeque<Char>): ElfStacks {
+            val elfStacks = ElfStacks()
+            elfStacks.stacks = stacks.toList()
+            return elfStacks
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ElfStacks
+
+        if (stacks != other.stacks) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return stacks.hashCode()
+    }
+
+    override fun toString(): String {
+        return "ElfStacks(stacks=$stacks)"
+    }
+
+    fun peekAll(): List<Char> = stacks.map { it.last() }
+    fun makeStackMove(move: StackMove): Unit {
+        repeat(move.n) {
+            val item = stacks[move.from - 1].removeLast()
+            stacks[move.to - 1].addLast(item)
+        }
+    }
+}
+
+data class StackMove(val n: Int, val from: Int, val to: Int)
--- src/test/kotlin/com/ericrochester/advent2022/TestDay05.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay05.kt
@@ -10,12 +10,80 @@ class TestDay05 {
     @Test
     fun testDay05RunA() {
         val output = day.runA(inputData)
-        assertEquals(TODO(), output)
+        assertEquals("CMZ", output)
     }
 
     @Test
     fun testDay05RunB() {
         val output = day.runB(inputData)
         assertEquals(TODO(), output)
+    }
+
+    @Test
+    fun testParseStacks() {
+        val input = """
+            |    [D]
+            |[N] [C]
+            |[Z] [M] [P]
+            | 1   2   3
+            """.trimMargin().lines()
+        val elfStacks = day.parseStacks(input)
+        val expected = ElfStacks.withStacks(
+            ArrayDeque(listOf('Z', 'N')),
+            ArrayDeque(listOf('M', 'C', 'D')),
+            ArrayDeque(listOf('P'))
+        )
+        assertEquals(expected, elfStacks)
+    }
+
+    @Test
+    fun testParseStackMove() {
+        val input = "move 1 from 2 to 1"
+        val expected = StackMove(1, 2, 1)
+        assertEquals(expected, day.parseStackMove(input))
+    }
+
+    @Test
+    fun testMakeStackMoveSingle() {
+        val stacks = ElfStacks.withStacks(
+            ArrayDeque(listOf('Z', 'N')),
+            ArrayDeque(listOf('M', 'C', 'D')),
+            ArrayDeque(listOf('P'))
+        )
+        val move = StackMove(1, 2, 1)
+        val expected = ElfStacks.withStacks(
+            ArrayDeque(listOf('Z', 'N', 'D')),
+            ArrayDeque(listOf('M', 'C')),
+            ArrayDeque(listOf('P'))
+        )
+        stacks.makeStackMove(move)
+        assertEquals(expected, stacks)
+    }
+
+    @Test
+    fun testMakeStackMoveMultiple() {
+        val stacks = ElfStacks.withStacks(
+            ArrayDeque(listOf('Z', 'N', 'D')),
+            ArrayDeque(listOf('M', 'C')),
+            ArrayDeque(listOf('P'))
+        )
+        val move = StackMove(3, 1, 3)
+        val expected = ElfStacks.withStacks(
+            ArrayDeque(listOf()),
+            ArrayDeque(listOf('M', 'C')),
+            ArrayDeque(listOf('P', 'D', 'N', 'Z'))
+        )
+        stacks.makeStackMove(move)
+        assertEquals(expected, stacks)
+    }
+
+    @Test
+    fun testPeekAll() {
+        val input = ElfStacks.withStacks(
+            ArrayDeque(listOf('Z', 'N')),
+            ArrayDeque(listOf('M', 'C', 'D')),
+            ArrayDeque(listOf('P'))
+        )
+        assertEquals(listOf('N', 'D', 'P'), input.peekAll())
     }
 }
