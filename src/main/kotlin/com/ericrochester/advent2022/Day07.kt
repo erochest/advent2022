ab4dc492221f3f1b07ffcf2fa282e5d2e7cf72c1
Eric Rochester <erochest@gmail.com>
2022-12-07 09:47:52 -0500

    day 7 a

--- src/main/kotlin/com/ericrochester/advent2022/Day07.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day07.kt
@@ -1,12 +1,71 @@
 package com.ericrochester.advent2022
 
 class Day07: DayRuns<Int, Int> {
-    override fun runA(inputData: String): Int {
-        TODO("Not yet implemented")
-    }
+    override fun runA(inputData: String): Int =
+        inputData.lines()
+            .fold(DirectoryBuilder()) { builder, line ->
+                builder.exec(line)
+                builder
+            }
+            .allDirectories()
+            .filter { it.size <= 100000 }
+            .sumOf { it.size }
 
     override fun runB(inputData: String): Int {
         TODO("Not yet implemented")
     }
 
+}
+
+class DirectoryBuilder() {
+    val tree = Directory("/", null)
+    var current = tree
+
+    fun exec(command: String) {
+        if (command == "$ ls") {
+        } else if (command == "$ cd /") {
+            current = tree
+        } else if (command == "$ cd ..") {
+            current = current.parent ?: tree
+        } else if (command.startsWith("$ cd ")) {
+            val name = command.substring(5)
+            val child = Directory(name, current)
+            current.children.put(name, child)
+            current = child
+        } else if (command.startsWith("dir ")) {
+            val name = command.split("\\s+".toRegex())[1]
+            current.children[name] = Directory(name, current)
+        } else {
+            val parts = command.split("\\s+".toRegex())
+            val size = parts[0].toInt()
+            val name = parts[1]
+            val fileInfo = FileInfo(name, size)
+            current.files[name] = fileInfo
+        }
+    }
+
+    fun allDirectories(): List<Directory> {
+        val accum = mutableListOf<Directory>()
+        val queue = ArrayDeque<Directory>()
+        queue.add(tree)
+
+        while (queue.size > 0) {
+            val cursor = queue.removeLast()
+            accum.add(cursor)
+            queue.addAll(cursor.children.values)
+        }
+
+        return accum.toList()
+    }
+}
+
+data class FileInfo(val name: String, val size: Int)
+
+data class Directory(val name: String, val parent: Directory?) {
+    val children = mutableMapOf<String, Directory>()
+    val files = mutableMapOf<String, FileInfo>()
+
+    val size: Int
+        get() =
+            children.values.sumOf { it.size } + files.values.sumOf { it.size }
 }
--- src/test/kotlin/com/ericrochester/advent2022/TestDay07.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay07.kt
@@ -1,21 +1,126 @@
 package com.ericrochester.advent2022
 
 import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class TestDay07 {
     private val inputData = javaClass.getResource("/examples/day07.txt")?.readText() ?: ""
     private val day = Day07()
 
+     @Test
+     fun testDay07RunA() {
+         val output = day.runA(inputData)
+         assertEquals(95437, output)
+     }
+
+     @Test
+     fun testDay07RunB() {
+         val output = day.runB(inputData)
+         assertEquals(TODO(), output)
+     }
+
     @Test
-    fun testDay07RunA() {
-        val output = day.runA(inputData)
-        assertEquals(TODO(), output)
+    fun testParseCdRoot() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd /")
+        assertEquals("/", builder.current.name)
+        assertEquals("/", builder.tree.name)
+        assertContentEquals(listOf(), builder.tree.children.values)
+        assertContentEquals(listOf(), builder.tree.files.values)
     }
 
     @Test
-    fun testDay07RunB() {
-        val output = day.runB(inputData)
-        assertEquals(TODO(), output)
+    fun testCdRootFromCdild() {
+        val builder = DirectoryBuilder()
+        val directoryA = Directory("a", builder.tree)
+        builder.tree.children.put("a", directoryA)
+        builder.current = directoryA
+        builder.exec("$ cd /")
+        assertEquals("/", builder.current.name)
+    }
+
+    @Test
+    fun testParseCdChild() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd /")
+        builder.exec("$ cd a")
+        assertEquals("a", builder.current.name)
+        assertContains(builder.tree.children.values, Directory("a", builder.tree))
+    }
+
+    @Test
+    fun testAddDirectoryOnlyOnce() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd /")
+        builder.exec("$ cd a")
+        builder.exec("$ cd /")
+        builder.exec("$ cd a")
+        assertContains(builder.current.children.values, Directory("a", builder.tree))
+        assertEquals(1, builder.tree.children.size)
+    }
+
+    @Test
+    fun testParseCdParent() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd /")
+        builder.exec("$ cd a")
+        builder.exec("$ cd b")
+        builder.exec("$ cd ..")
+        assertEquals("a", builder.current.name)
+    }
+
+    @Test fun testParseLsFileList() {
+        val builder = DirectoryBuilder()
+        builder.exec("14848514 b.txt")
+        assertEquals(1, builder.tree.files.size)
+        assertContains(builder.tree.files.keys, "b.txt")
+        assertContains(builder.tree.files.values, FileInfo("b.txt", 14848514))
+    }
+
+    @Test fun testParseLsChildDirectory() {
+        val builder = DirectoryBuilder()
+        builder.exec("dir a")
+        assertEquals(1, builder.tree.children.size)
+        assertContains(builder.tree.children.keys, "a")
+        val child = builder.tree.children["a"]
+        assertEquals("a", child?.name ?: "")
+        assertEquals(builder.current, child?.parent)
+    }
+
+    @Test fun testDirectorySize() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd e")
+        builder.exec("$ ls")
+        builder.exec("584 i")
+        val e = builder.current
+        assertEquals(584, e.size)
+    }
+
+    @Test fun testdirectorytreesize() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ ls")
+        builder.exec("14848514 b.txt")
+        builder.exec("8504156 c.dat")
+        builder.exec("$ cd e")
+        builder.exec("$ ls")
+        builder.exec("584 i")
+        assertEquals(584+14848514+8504156, builder.tree.size)
+    }
+
+    @Test fun testAllDirectories() {
+        val builder = DirectoryBuilder()
+        builder.exec("$ cd e")
+        builder.exec("$ ls")
+        builder.exec("584 i")
+        builder.exec("$ cd ..")
+        builder.exec("$ cd a")
+        builder.exec("$ cd b")
+        builder.exec("$ cd ..")
+        builder.exec("$ cd c")
+        val directories = builder.allDirectories().map { it.name }.toSet()
+        assertEquals(setOf("/", "a", "b", "c", "e"), directories)
     }
 }
