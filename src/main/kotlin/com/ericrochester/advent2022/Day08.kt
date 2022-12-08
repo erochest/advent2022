1021f36a5072439d0af9a165b38c9867159e84fd
Eric Rochester <erochest@gmail.com>
2022-12-08 17:50:33 -0500

    day 8

--- src/main/kotlin/com/ericrochester/advent2022/Day08.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day08.kt
@@ -2,11 +2,97 @@ package com.ericrochester.advent2022
 
 class Day08: DayRuns<Int, Int> {
     override fun runA(inputData: String): Int {
-        TODO("Not yet implemented")
+        val heights = inputData.lines()
+            .map { it.map { c -> c.toString().toInt() } }
+        val gridHeight = heights.size
+        val gridWidth = heights[0].size
+        var hidden = 0
+
+        for (r in 0 until gridHeight) {
+            for (c in 0 until gridWidth) {
+//                println("${r}x${c} | ${isVisible(heights, r, c)}")
+                if (isVisible(heights, r, c)) {
+                    hidden += 1
+                }
+            }
+        }
+
+        return hidden
     }
 
     override fun runB(inputData: String): Int {
-        TODO("Not yet implemented")
+        val heights = inputData.lines()
+            .map { it.map { c -> c.toString().toInt() } }
+        val gridHeight = heights.size
+        val gridWidth = heights[0].size
+        var maxScore = 0
+
+        for (r in 0 until gridHeight) {
+            for (c in 0 until gridWidth) {
+//                println("${r}x${c} | ${isVisible(heights, r, c)}")
+                maxScore = maxOf(maxScore, scenicScore(heights, r, c))
+            }
+        }
+
+        return maxScore
+    }
+
+    fun isVisible(heights: List<List<Int>>, r: Int, c: Int): Boolean {
+        val gridHeight = heights.size
+        val gridWidth = heights[0].size
+        val treeHeight = heights[r][c]
+
+        if (r == 0 || r == gridHeight - 1 || c == 0 || c == gridWidth - 1) {
+            return true
+        }
+
+        val fromTop = (0 until r).map { heights[it][c] }.all { it < treeHeight }
+        val fromBottom = ((r + 1) until gridHeight).map { heights[it][c] }.all { it < treeHeight }
+
+        val fromLeft = heights[r].subList(0, c).all { it < treeHeight }
+        val fromRight = heights[r].subList(c + 1, gridWidth).all { it < treeHeight }
+
+        return fromTop || fromBottom || fromRight || fromLeft
+    }
+
+    fun scenicScore(heights: List<List<Int>>, r: Int, c: Int): Int {
+        val gridHeight = heights.size
+        val gridWidth = heights[0].size
+        val treeHeight = heights[r][c]
+
+        var left = 0
+        for (c1 in (c-1) downTo 0) {
+            left += 1
+            if (heights[r][c1] >= treeHeight) {
+                break
+            }
+        }
+
+        var above = 0
+        for (r1 in (r-1) downTo 0) {
+            above += 1
+            if (heights[r1][c] >= treeHeight) {
+                break
+            }
+        }
+
+        var right = 0
+        for (c1 in (c+1) until gridWidth) {
+            right += 1
+            if (heights[r][c1] >= treeHeight) {
+                break
+            }
+        }
+
+        var below = 0
+        for (r1 in (r+1) until gridHeight) {
+            below += 1
+            if (heights[r1][c] >= treeHeight) {
+                break
+            }
+        }
+
+        return above * left * right * below
     }
 
 }
--- src/test/kotlin/com/ericrochester/advent2022/TestDay08.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay08.kt
@@ -2,21 +2,62 @@ package com.ericrochester.advent2022
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class TestDay08 {
     private val inputData = javaClass.getResource("/examples/day08.txt")?.readText() ?: ""
     private val day = Day08()
 
+    private val heights = listOf(
+        listOf(3, 0, 3, 7, 3),
+        listOf(2, 5, 5, 1, 2),
+        listOf(6, 5, 3, 3, 2),
+        listOf(3, 5 ,5, 4, 9),
+        listOf(3, 5, 3, 9, 0)
+    )
+
     @Test
     fun testDay08RunA() {
         val output = day.runA(inputData)
-        assertEquals(TODO(), output)
+        assertEquals(21, output)
     }
 
     @Test
     fun testDay08RunB() {
         val output = day.runB(inputData)
-        assertEquals(TODO(), output)
+        assertEquals(8, output)
     }
 
+    @Test fun testIsVisibleReturnsTrueOnEdges() {
+        assertTrue(day.isVisible(heights, 0, 3))
+        assertTrue(day.isVisible(heights, 4, 3))
+        assertTrue(day.isVisible(heights, 3, 0))
+        assertTrue(day.isVisible(heights, 3, 4))
+    }
+
+    @Test fun testIsVisibleReturnsFalseWhenHiddenAllDirections() {
+        assertFalse(day.isVisible(heights, 1, 3))
+        assertFalse(day.isVisible(heights, 2, 2))
+        assertFalse(day.isVisible(heights, 3, 3))
+    }
+
+    @Test fun testIsVisibleReturnsTrueIfSeenFromSides() {
+        // visible from the right
+        assertTrue(day.isVisible(heights, 2, 1))
+        assertTrue(day.isVisible(heights, 2, 3))
+    }
+
+    @Test fun testIsVisibleReturnsTrueIfSeenFromTopOrBottom() {
+        assertTrue(day.isVisible(heights, 1, 1))
+        assertTrue(day.isVisible(heights, 1, 2))
+        assertTrue(day.isVisible(heights, 2, 1))
+        assertTrue(day.isVisible(heights, 3, 2))
+    }
+
+    @Test fun testScenicScore() {
+        assertEquals(4, day.scenicScore(heights, 1, 2))
+        assertEquals(4, day.scenicScore(heights, 3, 2))
+        assertEquals(1, day.scenicScore(heights, 2, 2))
+    }
 }
