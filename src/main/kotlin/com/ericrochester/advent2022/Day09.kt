6739a8f3bc0c1245a97d0dbce6c6bd946a2809b7
Eric Rochester <erochest@gmail.com>
2022-12-09 07:41:36 -0500

    day 8 a

--- src/main/kotlin/com/ericrochester/advent2022/Day09.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day09.kt
@@ -1,12 +1,80 @@
 package com.ericrochester.advent2022
 
+import kotlin.math.abs
+import kotlin.math.sign
+
 class Day09: DayRuns<Int, Int> {
-    override fun runA(inputData: String): Int {
-        TODO("Not yet implemented")
-    }
+    override fun runA(inputData: String): Int =
+        inputData.lines()
+            .map { parseDirection(it) }
+            .fold(Board(Pair(0, 0), Pair(0, 0))) { board, dirPair ->
+                board.move(dirPair)
+                board
+            }
+            .tailTrack
+            .size
 
     override fun runB(inputData: String): Int {
         TODO("Not yet implemented")
+    }
+
+    fun parseDirection(input: String): DirectionPair {
+        val parts = input.split(" ")
+        val dir = when (parts[0]) {
+            "R"  -> Direction.Right
+            "L"  -> Direction.Left
+            "U"  -> Direction.Up
+            else -> Direction.Down
+        }
+        val dist = parts[1].toInt()
+        return DirectionPair(dir, dist)
+    }
+
+}
+
+data class DirectionPair(val direction: Direction, val distance: Int)
+
+enum class Direction {
+    Right,
+    Left,
+    Up,
+    Down
+}
+
+data class Board(var head: Pair<Int, Int>, var tail: Pair<Int, Int>) {
+    val tailTrack = mutableSetOf<Pair<Int, Int>>()
+
+    init {
+        tailTrack.add(tail)
+    }
+
+    fun move(direction: Direction) {
+        head = when (direction) {
+            Direction.Right -> head.copy(first=head.first+1)
+            Direction.Left -> head.copy(first=head.first-1)
+            Direction.Up -> head.copy(second=head.second+1)
+            Direction.Down -> head.copy(second=head.second-1)
+        }
+
+        if (abs(head.first - tail.first) > 1 || abs(head.second - tail.second) > 1) {
+            tail = if (head.first == tail.first) {
+                tail.copy(second=tail.second+(head.second-tail.second).sign)
+            } else if (head.second == tail.second) {
+                tail.copy(first=tail.first+(head.first-tail.first).sign)
+            } else {
+                Pair(
+                    tail.first+(head.first-tail.first).sign,
+                    tail.second+(head.second-tail.second).sign
+                )
+            }
+            tailTrack.add(tail)
+        }
+    }
+
+    fun move(directionPair: DirectionPair) {
+        repeat(directionPair.distance) {
+            move(directionPair.direction)
+        }
     }
 
 }
--- src/test/kotlin/com/ericrochester/advent2022/TestDay09.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay09.kt
@@ -1,6 +1,7 @@
 package com.ericrochester.advent2022
 
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class TestDay09 {
@@ -10,13 +11,59 @@ class TestDay09 {
     @Test
     fun testDay09RunA() {
         val output = day.runA(inputData)
-        assertEquals(TODO(), output)
+        assertEquals(13, output)
     }
 
     @Test
     fun testDay09RunB() {
         val output = day.runB(inputData)
         assertEquals(TODO(), output)
+    }
+
+    @Test fun testParseDirection() {
+        val direction = day.parseDirection("R 4")
+        assertEquals(DirectionPair(Direction.Right, 4), direction)
+    }
+
+    @Test fun testMoveDirection() {
+        val board = Board(Pair(0, 0), Pair(0, 0))
+        board.move(Direction.Right)
+        assertEquals(Pair(1, 0), board.head)
+    }
+
+    @Test fun testMoveTailFollowsRight() {
+        val board = Board(Pair(1, 0), Pair(0, 0))
+        board.move(Direction.Right)
+        assertEquals(Pair(2, 0), board.head)
+        assertEquals(Pair(1, 0), board.tail)
+    }
+
+    @Test fun MoveTailFollowsDiagonal() {
+        val board = Board(Pair(1, 1), Pair(0, 0))
+        board.move(Direction.Up)
+        assertEquals(Pair(1, 2), board.head)
+        assertEquals(Pair(1, 1), board.tail)
+    }
+
+    @Test fun testBoardTracksTail() {
+        val board = Board(Pair(1, 0), Pair(0, 0))
+        assertContains(board.tailTrack, Pair(0, 0))
+    }
+
+    @Test fun testMoveTracksTail() {
+        val board = Board(Pair(1, 0), Pair(0, 0))
+        board.move(Direction.Right)
+        assertContains(board.tailTrack, Pair(1, 0))
+    }
+
+    @Test fun testMoveDirectionPair() {
+        val board = Board(Pair(0, 0), Pair(0, 0))
+        board.move(DirectionPair(Direction.Right, 4))
+        assertEquals(Pair(4, 0), board.head)
+        assertEquals(Pair(3, 0), board.tail)
+        assertEquals(setOf(
+            Pair(0, 0), Pair(1, 0), Pair(2, 0), Pair(3, 0)
+        ), board.tailTrack)
     }
 
 }
