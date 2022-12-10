4b9c673c5a21f71b5d6867016169641e85a8df29
Eric Rochester <erochest@gmail.com>
2022-12-10 13:30:05 -0500

    day 10 a

--- src/main/kotlin/com/ericrochester/advent2022/Day10.kt
+++ src/main/kotlin/com/ericrochester/advent2022/Day10.kt
@@ -2,11 +2,52 @@ package com.ericrochester.advent2022
 
 class Day10: DayRuns<Int,Int> {
     override fun runA(inputData: String): Int {
-        TODO("Not yet implemented")
+        val history = execute(inputData.lines())
+        return (20..(history.size) step 40)
+            .sumOf { signalStrength(history, it) }
     }
 
     override fun runB(inputData: String): Int {
         TODO("Not yet implemented")
     }
 
+}
+
+data class Cpu(var register: Int) {
+    val history: MutableList<Int> = mutableListOf()
+
+    val output: List<Int>
+        get() {
+            val output = mutableListOf<Int>()
+            output.addAll(history)
+            output.add(register)
+            return output.toList()
+        }
+
+    fun noop() {
+        history.add(register)
+    }
+
+    fun addx(x: Int) {
+        history.add(register)
+        history.add(register)
+        register += x
+    }
+}
+
+fun execute(commands: List<String>): List<Int> {
+    return commands.fold(Cpu(1)) { cpu, command ->
+        if (command == "noop") {
+            cpu.noop()
+        } else {
+            val parts = command.split(' ')
+            val value = parts[1].toInt()
+            cpu.addx(value)
+        }
+        cpu
+    }.output
+}
+
+fun signalStrength(cpuOutput: List<Int>, cycle: Int): Int {
+    return cycle * cpuOutput[cycle - 1]
 }
--- src/test/kotlin/com/ericrochester/advent2022/TestDay10.kt
+++ src/test/kotlin/com/ericrochester/advent2022/TestDay10.kt
@@ -8,13 +8,56 @@ class TestDay10 {
     private val inputData = javaClass.getResource("/examples/day10.txt")?.readText() ?: ""
     private val day = Day10()
 
-    @Ignore @Test fun testDay10RunA() {
+    @Test fun testDay10RunA() {
         val output = day.runA(inputData)
-        assertEquals(TODO(), output)
+        assertEquals(13140, output)
     }
 
     @Ignore @Test fun testDay10RunB() {
         val output = day.runB(inputData)
         assertEquals(TODO(), output)
+    }
+
+    @Test fun testExecuteStartsWithEmptyList() {
+        val output = execute(listOf())
+        assertEquals(listOf(1), output)
+    }
+
+    @Test fun testExecuteNoopDoesNothingForASecond() {
+        val output = execute(listOf("noop"))
+        assertEquals(listOf(1, 1), output)
+    }
+
+    @Test fun testExecuteAddX() {
+        val output = execute(listOf("addx 3"))
+        assertEquals(listOf(1, 1, 4), output)
+    }
+
+    @Test fun testExecuteProgram() {
+        val input = listOf("noop", "addx 3", "addx -5")
+        val output = execute(input)
+        val expected = listOf(1, 1, 1, 4, 4, -1)
+        assertEquals(expected, output)
+    }
+
+    @Test fun cpuNoop() {
+        val cpu = Cpu(1)
+        cpu.noop()
+        assertEquals(1, cpu.register)
+        assertEquals(listOf(1), cpu.history)
+    }
+
+    @Test fun cpuAddX() {
+        val cpu = Cpu(1)
+        cpu.addx(3)
+        assertEquals(4, cpu.register)
+        assertEquals(listOf(1, 1), cpu.history)
+    }
+
+    @Test fun signalStrength() {
+        val cpuOutput = listOf(1, 1, 1, 4, 4, -1)
+        assertEquals(16, signalStrength(cpuOutput, 4))
+        assertEquals(20, signalStrength(cpuOutput, 5))
+        assertEquals(-6, signalStrength(cpuOutput, 6))
     }
 }
